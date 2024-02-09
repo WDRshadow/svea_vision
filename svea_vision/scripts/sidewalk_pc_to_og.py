dbb4ad7a5d0ca811295c8c76abfbb76d152440ef
Sulthan Suresh Fazeela <sulthansf95@gmail.com>
2024-02-09 16:33:31 +0100

    Sidewalk occupancy grid: transform to base frame

--- svea_vision/scripts/sidewalk_pc_to_og.py
+++ svea_vision/scripts/sidewalk_pc_to_og.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import rospy
+import tf2_ros
+from tf2_sensor_msgs.tf2_sensor_msgs import do_transform_cloud
 from sensor_msgs.msg import PointCloud2
 from nav_msgs.msg import OccupancyGrid
 
@@ -37,27 +39,33 @@ class SidewalkPointCloudToOccupancyGrid:
             self.sidewalk_occupancy_grid_topic = load_param('~sidewalk_occupancy_grid_topic', 'sidewalk_occupancy_grid')
             
             # Sidewalk parameters
-            self.sidewalk_z_min = load_param('~sidewalk_z_min', -0.5)
-            self.sidewalk_z_max = load_param('~sidewalk_z_max', 0.5)
-            self.obstacle_z_min = load_param('~obstacle_z_min', 0.5)
+            self.sidewalk_z_min = load_param('~sidewalk_z_min', -0.2)
+            self.sidewalk_z_max = load_param('~sidewalk_z_max', 0.2)
+            self.obstacle_z_min = load_param('~obstacle_z_min', 0.2)
             self.obstacle_z_max = load_param('~obstacle_z_max', 2.0)
             
             # Occupancy grid parameters
+            self.base_frame = load_param('~base_frame', 'base_link')
             self.resolution = load_param('~resolution', 0.05)
-            self.width = load_param('~width', 100)
-            self.height = load_param('~height', 100)
+            self.width = load_param('~width', 20)
+            self.height = load_param('~height', 20)
             self.occupied_value = load_param('~occupied_value', 100)
             self.free_value = load_param('~free_value', 0)
             self.unknown_value = load_param('~unknown_value', -1)
             
             # Initialize occupancy grid
             self.sidewalk_occupancy_grid = OccupancyGrid()
+            self.sidewalk_occupancy_grid.header.frame_id = self.base_frame
             self.sidewalk_occupancy_grid.info.resolution = self.resolution
             self.sidewalk_occupancy_grid.info.width = int(self.width/self.resolution)
             self.sidewalk_occupancy_grid.info.height = int(self.height/self.resolution)
             # Set world point (0, 0) to be the center of the grid
             self.sidewalk_occupancy_grid.info.origin.position.x = -self.width/2
             self.sidewalk_occupancy_grid.info.origin.position.y = -self.height/2
+            
+            # TF2
+            self.tf_buf = tf2_ros.Buffer()
+            self.tf_listener = tf2_ros.TransformListener(self.tf_buf)
             
             # Publishers
             self.sidewalk_occupancy_grid_pub = rospy.Publisher(self.sidewalk_occupancy_grid_topic, OccupancyGrid, queue_size=1)
@@ -80,11 +88,24 @@ class SidewalkPointCloudToOccupancyGrid:
             rospy.loginfo('Shutting down {}'.format(rospy.get_name()))
             
     def pointcloud_callback(self, msg):
+        # Transform pointcloud to base frame
+        if not self.base_frame:
+            rospy.logerr('{}: base_frame not set'.format(rospy.get_name()))
+            return
+        if self.base_frame != msg.header.frame_id:
+            try:
+                transform_stamped = self.tf_buf.lookup_transform(self.base_frame, msg.header.frame_id, msg.header.stamp)
+            except tf2_ros.LookupException:
+                rospy.logwarn("{}: Transform lookup from {} to {} failed for the requested time. Using latest transform instead.".format(rospy.get_name(), msg.header.frame_id, self.base_frame))
+                transform_stamped = self.tf_buf.lookup_transform(self.base_frame, msg.header.frame_id, rospy.Time(0))
+            msg = do_transform_cloud(msg, transform_stamped)                
+        
         # Convert pointcloud to numpy array
         pointcloud_data = ros_numpy.numpify(msg)
         pointcloud_data = ros_numpy.point_cloud2.get_xyz_points(pointcloud_data)
         
         # Create occupancy grid
+        self.sidewalk_occupancy_grid.header.stamp = msg.header.stamp
         self.sidewalk_occupancy_grid.data = self.create_occupancy_grid(pointcloud_data)
         
         # Publish occupancy grid
@@ -118,6 +139,7 @@ class SidewalkPointCloudToOccupancyGrid:
         return occupancy_grid.flatten(order='F').tolist()   
     
     def world_to_grid(self, x, y):
+        # Convert world point to grid cell
         i = int((x - self.sidewalk_occupancy_grid.info.origin.position.x) / self.sidewalk_occupancy_grid.info.resolution)
         j = int((y - self.sidewalk_occupancy_grid.info.origin.position.y) / self.sidewalk_occupancy_grid.info.resolution)
         
