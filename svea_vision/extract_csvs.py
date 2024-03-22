1580752f694ec071f4757c7f64cf0547d418d1e9
kfragkedaki <fr-claire@hotmail.com>
2024-03-22 11:02:07 +0100

    update extract_csvs file

--- svea_vision/extract_csvs.py
+++ svea_vision/extract_csvs.py
@@ -1,14 +1,35 @@
 import rosbag
 import pandas as pd
+import os
+import rospy_message_converter as converter
 
 CURRENT_FILE = './src/svea_vision/svea_vision/'
 file_paths = ["out_2024-03-04-19-08-30_one_person_moving", "out_2024-03-04-19-09-45_standing", 
               "out_2024-03-04-19-10-28_two_people_moving", "out_2024-03-04-19-11-38_one_person_standing_one_moving", 
               "out_2024-03-04-19-13-21"]
 
-for path in file_paths:
+
+def create_dirs(dirs):
+    """
+    Input:
+        dirs: a list of directories to create, in case these directories are not found
+    Returns:
+        exit_code: 0 if success, -1 if failure
+    """
+    try:
+        for dir_ in dirs:
+            if not os.path.exists(dir_):
+                os.makedirs(dir_)
+        return 0
+    except Exception as err:
+        print("Creating directories error: {0}".format(err))
+        exit(-1)
+
+def save_data(path):
+    create_dirs(path)
+
     # The bag file should be in the same directory as your terminal
-    bag = rosbag.Bag(CURRENT_FILE + path + '.bag')
+    bag = rosbag.Bag(path + '.bag')
     topics = ['/objectposes', '/person_state_estimation/person_states', '/qualisys/pedestrian/pose', '/qualisys/pedestrian/velocity', '/qualisys/tinman/pose', '/qualisys/tinman/velocity']
     column_names = ['topic', 'seq', 'stamp_secs', 'stamp_nsecs',
         'positionx', 'positiony', 'positionz', 
@@ -16,12 +37,15 @@ for path in file_paths:
     df = pd.DataFrame(columns=column_names)
 
     for topic, msg, t in bag.read_messages(topics=topics):
-        print(msg)
-        print(topic)
-        print('\n')
         seq = msg.header.seq
         stamp_secs = msg.header.stamp.secs
         stamp_nsecs = msg.header.stamp.nsecs
+
+        d = converter.convert_ros_message_to_dictionary(msg)
+        print(d)
+        print(topic)
+        print('\n')
+
 
         if '/pose' in topic:
             positionx = msg.pose.position.x
@@ -82,3 +106,8 @@ for path in file_paths:
         df = pd.concat([df, dataset])
 
     df.to_csv(CURRENT_FILE + path + '.csv')
+
+
+if __name__ == "__main__":
+    for path in file_paths:
+        save_data(CURRENT_FILE + path)
